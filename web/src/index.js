676e7ac643863e98f823904c00901326e998ac53
R-KL <richardkliju@gmail.com>
2026-05-18 11:59:30 +0530

    Idk what happend, git merging messedup, this is the fix for that

--- web/src/index.js
+++ web/src/index.js
@@ -704,17 +704,12 @@ Alpine.data("AceApp", () => ({
   },
   async readFromUrl() {
     const hash = window.location.hash.slice(1);
-<<<<<<< HEAD
     if (!hash) {
       this.loading = false;
       return;
     }
     try {
-=======
-    if (!hash) return;
-    try {
       this.loading = true;
->>>>>>> 0525e199a8003a91201fff417e3c917f16919e39
       const decoded = lz.decompressFromEncodedURIComponent(hash);
       const [, encodedcode, mode, theme, render] = decoded.match(
         /\?code=([^&]*)&mode=([^&]*)&theme=([^&]*)&render=([^&]*)/,
@@ -736,10 +731,7 @@ Alpine.data("AceApp", () => ({
           createPreview("Markdown Preview",false);
           renderPreviewMarkdown(code);
         }
-<<<<<<< HEAD
         this.loading = false;
-=======
->>>>>>> 0525e199a8003a91201fff417e3c917f16919e39
         setTimeout(() => {
           this.$dispatch("update-msg", {
             msg: `Decompresing #${hash.slice(0, 20)}...`,
@@ -751,14 +743,10 @@ Alpine.data("AceApp", () => ({
       }
     } catch (e) {
       console.warn("Failed to decompress content from URL:", e);
-<<<<<<< HEAD
-    } finally {
+    }
+    finally {
       this.loading = false;
-    }
-=======
-    }
-    this.loading = false;
->>>>>>> 0525e199a8003a91201fff417e3c917f16919e39
+      }
   },
   markDownMode() {
     // this includes both markdown and html since hey they both can use the marked preview
